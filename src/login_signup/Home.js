4555579dc197e6c8fe60b950300446ec024c44b7
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-05-07 00:56:51 +0530

    hash

--- src/login_signup/Home.js
+++ src/login_signup/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Shop_owner/login/Login";
 import Register from "./Shop_owner/register/Register";
 function Home() {
