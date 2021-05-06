5cf7d7a02ae7e1ef1fe19922a325a1d532bcde65
Pratik-Kumar-621 <70286186+Pratik-Kumar-621@users.noreply.github.com>
2021-05-07 01:25:11 +0530

    structure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Auth from "./User/auth/auth";
+import Cart from "./User/cart/cart";
+import Home from "./User/Home/home";
+import Profile from "./User/profile/profile";
+import Shop from "./User/shop/shop";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <h1>Home</h1>
+          <Home />
+        </Route>
+        <Route exact path="/shop">
+          <Shop />
+        </Route>
+        <Route exact path="/auth">
+          <Auth />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
         </Route>
       </Switch>
     </Router>
