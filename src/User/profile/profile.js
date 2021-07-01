1aab966158026f9186743541e62ed6c514656460
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2021-07-02 00:45:50 +0530

    Some more commit of file system

--- src/User/profile/profile.js
+++
@@ -1,9 +0,0 @@
-import Orders from './Orders/Orders';
-import { jayesh } from '../../Data/customers/customer-imdivisual-data'
-const Profile = () => {
-  return (
-    <Orders userID={jayesh}/>
-  );
-};
-
-export default Profile;
