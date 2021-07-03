d2d2286831aa0c0b515f42298373b9b4622f8361
Abhishek kumar <63067796+Abhishek1342@users.noreply.github.com>
2021-07-04 00:54:28 +0530

    Fixed the layout of searchbar

--- Client/src/User/home/components/Searchbar.jsx
+++ Client/src/User/home/components/Searchbar.jsx
@@ -6,24 +6,25 @@ import "./Searchbar.css";
 function Searchbar(props) {
    return (
       <div className="searchbar-container">
-         <a href="/#" className="location-btn">
-            Location
-            <LocationOnIcon style={{ color: "#F44336" }} />
-         </a>
-         <input
-            className="searchbox"
-            type="text"
-            placeholder={props.placeholder}
-            onChange={props.onChange}
-         />
-         <a
-            href="/#"
-            type="submit"
-            onClick={props.onClick}
-            className="search-btn"
-         >
-            <SearchIcon style={{ fontSize: "35px" }} />
-         </a>
+         <div>
+            <button className="location-btn">
+               Location
+               <LocationOnIcon style={{ color: "#F44336" }} />
+            </button>
+            <input
+               className="searchbox"
+               type="text"
+               placeholder={props.placeholder}
+               onChange={props.onChange}
+            />
+            <button
+               type="submit"
+               onClick={props.onClick}
+               className="search-btn"
+            >
+               <SearchIcon style={{ fontSize: "35px" }} />
+            </button>
+         </div>
       </div>
    );
 }
