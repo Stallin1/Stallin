65b178f52e518ceaffd8a8dd23d64bc82558b374
ahkhan13 <khanahs3351@gmail.com>
2021-07-07 00:30:34 -0700

    register and login backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require('body-parser');
 const app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 const dotenv = require("dotenv");
-const port = 8000;
-
 dotenv.config({ path: "./config.env" });
-
+const PORT = process.env.PORT;
 require("./db/conn");
-
+app.use(require('./router/app'));
 app.get("/", (req, res) => {
    res.send("hi");
 });
-
-app.listen(port, () => {
-   console.log(`Server is running on ${port} `);
+app.listen(PORT, () => {
+   console.log(`Server is running on ${PORT}`);
 });
